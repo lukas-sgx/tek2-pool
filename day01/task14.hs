635b9e2df0b80dfbb6511f5c8c3257289622710b
lukas-sgx <lukas.soigneux@epitech.eu>
2026-02-20 10:42:43 +0100

    Revert "fix(task14): not necessary myLenght"
    This reverts commit 3deebddf7ab582088570b832ff940e4ef5ab5fa9.

--- day01/task14.hs
+++ day01/task14.hs
@@ -1,3 +1,7 @@
+myLenght :: [a] -> Int
+myLenght [] = 0
+myLenght (s:e) = 1 + myLenght e
+
 myNth :: [a] -> Int -> a
 myNth (_) i | i < 0 = error "index negative"
 myNth [] _ = error "index too large"
