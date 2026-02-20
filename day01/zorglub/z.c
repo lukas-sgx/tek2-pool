3a8cea6f265548281b994570d7f19d2ceb176d94
lukas-sgx <lukas.soigneux@epitech.eu>
2026-02-20 17:29:37 +0100

    [+] Add zorglub

---
+++ day01/zorglub/z.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2026
+** ~/epitech/free-project/tek2-pool/day01
+** File description:
+** z
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("z\n");
+    return 0;
+}
